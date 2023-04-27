103211f5eabfdeda2a85e02c7ab6786e15fe1cd3
name <ksyusha0kubay@gmail.com>
2023-04-27 23:37:30 +0300

    Add models to social_media

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,60 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to="post_images/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    count_likes = models.PositiveIntegerField(default=0)
+    liked_by = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes", blank=True)
+    count_comments = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return (
+            f"author: {self.author}, content: {self.content}"
+            f" created_at: {self.created_at.strftime('%Y-%m-%d %H:%M')}"
+        )
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=150, blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    follower = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followers")
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return (
+            f"profile: {self.user}, bio: {self.bio}"
+            f" followers: {self.followers}"
+        )
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"user: {self.user}, liked post: {self.post.content}, "
+            f"at: {self.created_at.strftime('%Y-%m-%d %H:%M')}"
+        )
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"user: {self.user}, commented post {self.post.content},"
+            f" content: {self.content} at {self.created_at.strftime('%Y-%m-%d %H:%M')}"
+        )
