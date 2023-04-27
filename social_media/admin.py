d572663035509ab4c82937d3e3508a34a75bfe6c
name <ksyusha0kubay@gmail.com>
2023-04-27 23:41:19 +0300

    Add admin functional to social_media

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import Post, Like, Comment, Profile
+
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Profile)
